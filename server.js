9b8c638e135d89f4b4e81fae4fa0efd7ceba7889
GerizimJosh <gerizimcolosaga@gmail.com>
2026-03-11 22:29:01 +0800

    Changed PORT

--- server.js
+++ server.js
@@ -37,4 +37,8 @@ io.on("connection", socket => {
     socket.emit("slide-update", slideState);
 });
 
-server.listen(3000, '0.0.0.0', () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port", PORT);
+});
