a4f9e5183d7ecffbb6c9e0cef34c55e320623326
GJDevTech <135478189+GJDevTech@users.noreply.github.com>
2026-03-11 16:37:11 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(express.static("public"));
+
+// store latest slide data
+let slideState = { SongVerses: [], CurrentItemIndex: 1, Title: "" };
+
+// endpoint VideoPsalm posts to
+app.post("/update-slide", (req, res) => {
+    console.log(req.body);
+    slideState = req.body;
+    io.emit("slide-update", slideState); // broadcast to all clients
+    res.json({ success: true });
+});
+
+// stage-view (no chords)
+app.get("/stage-view", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/stage-view.html"));
+});
+
+// chords-view (with chords)
+app.get("/chords-view", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/chords-view.html"));
+});
+
+io.on("connection", socket => {
+    socket.emit("slide-update", slideState);
+});
+
+server.listen(3000, '0.0.0.0', () => console.log("Server running on port 3000"));
