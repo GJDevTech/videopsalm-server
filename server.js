024a83b3317a57ae40c708fa5cf9e6fa956318f3
GerizimJosh <gerizimcolosaga@gmail.com>
2026-03-11 16:54:01 +0800

    Added cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
